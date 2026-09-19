4d663d2e58bd062e26b66bcc8cc133f5446a45a1
Moein Siami <moeins1216@gmail.com>
2026-09-19 20:56:14 +0330

    second commit

---
+++ frontend/reservation/app/page.tsx
@@ -0,0 +1,15 @@
+// import Image from "next/image";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1 className="text-3xl underline">Hellooooo world!</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
